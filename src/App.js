4f98bf8a90e26155843e0e8bfcf65c237fd7d09e
Mesa Perry <mesperrus@gmail.com>
2020-02-17 00:16:32 -0600

    fixed domain

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Router>
+      <Router basename="/">
         <Switch>
           <Route path="/experience">
             <Experience />

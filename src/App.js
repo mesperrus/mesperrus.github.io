488b02a9689d82eec8a2aae4d9d0255028a0810d
Mesa Perry <mesperrus@gmail.com>
2020-05-18 23:42:12 -0500

    transition restructure

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class Container extends React.Component {
   // }
 
   render() {
-    this.setState({});
+    // this.setState({});
     return (
       <div className="color1 container">
         <div className="color2 inner">
@@ -44,40 +44,27 @@ class Container extends React.Component {
             <NavigationBar />
             <h1 className="text_color1 title">Experience</h1>
           </div>
-          <Switch>
-            <Route path="/experience">
-              <Experience />
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio />
-            </Route>
-            <Route path="/">
-              <About />
-            </Route>
-          </Switch>
+          <TransitionGroup>
+            <CSSTransition
+              timeout={{ enter: 300, exit: 300 }}
+              classNames={'fade'}
+            >
+              <Switch>
+                <Route path="/experience">
+                  <Experience />
+                </Route>
+                <Route path="/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/">
+                  <About />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
           <SocialBar />
         </div>
       </div>
-
-
-      <TransitionGroup>
-        <CSSTransition
-          timeout={{ enter: 300, exit: 300 }}
-          classNames={'fade'}
-        >
-          <Switch>
-            <Route path="/experience">
-              <Experience />
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio />
-            </Route>
-            <Route path="/">
-              <About />
-            </Route>
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
     );
   }
 }
@@ -134,29 +121,22 @@ class SocialBar extends React.Component {
 class About extends React.Component {
   render() {
     return (
-      <div className="color1 container">
-        <div className="color2 inner">
-          <div className="vertical">
-            <NavigationBar />
-            <h1 className="text_color1 title">Mesa Perry</h1>
-          </div>
-          <p className="info">
-            Heyo -- welcome to my abode!
-            I'm a student and software developer from
-            University of Illinois Urbana-Champaign.
-          </p>
-          <p className="info">
-            My experience includes a deep learning internship at
-            ST Engineering and various personal projects.
-            Recently I participated in HackIllinois 2020 and
-            won first place for Checkbook's challenge.
-            This summer I'll be interning as a SDE intern
-            at Amazon Lab126.
-            Feel free to visit some of my work above,
-            or check me out on other sites below.
-          </p>
-          <SocialBar />
-        </div>
+      <div className="vertical">
+        <p className="info">
+          Heyo -- welcome to my abode!
+          I'm a student and software developer from
+          University of Illinois Urbana-Champaign.
+        </p>
+        <p className="info">
+          My experience includes a deep learning internship at
+          ST Engineering and various personal projects.
+          Recently I participated in HackIllinois 2020 and
+          won first place for Checkbook's challenge.
+          This summer I'll be interning as a SDE intern
+          at Amazon Lab126.
+          Feel free to visit some of my work above,
+          or check me out on other sites below.
+        </p>
       </div>
     );
   }
@@ -165,17 +145,10 @@ class About extends React.Component {
 class Experience extends React.Component {
   render() {
     return (
-      <div className="color1 container">
-        <div className="color2 inner">
-          <div className="vertical">
-            <NavigationBar />
-            <h1 className="text_color1 title">Experience</h1>
-          </div>
-          <p className="info">
-            Coming soon...
-          </p>
-          <SocialBar />
-        </div>
+      <div>
+        <p className="info">
+          Coming soon...
+        </p>
       </div>
     );
   }
@@ -184,17 +157,10 @@ class Experience extends React.Component {
 class Portfolio extends React.Component {
   render() {
     return (
-      <div className="color1 container">
-        <div className="color2 inner">
-          <div className="vertical">
-            <NavigationBar />
-            <h1 className="text_color1 title">Portfolio</h1>
-          </div>
-          <p className="info">
-            Coming soon...
-          </p>
-          <SocialBar />
-        </div>
+      <div>
+        <p className="info">
+          Coming soon...
+        </p>
       </div>
     );
   }
